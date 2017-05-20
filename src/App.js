7f84ebea7f9d4b8925bdd8bd72e15830b7c8367c
Justin Barclay <justincbarclay@gmail.com>
2017-05-20 02:45:41 -0600

    Reconjiggering the UI framework

--- src/App.js
+++ src/App.js
@@ -1,80 +1,19 @@
 import React, { Component } from 'react';
-import './App.css';
-import Login from './login.js';
-import Header from './header.js';
-import LandingPage from './landingPage.js'
-
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import  Full from './containers/Full';
+import  Simple from './containers/Simple';
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {loggedIn: false};
-    this.login = this.login.bind(this);
-  }
-  login(success){
-    this.setState({loggedIn: success})  
-  }
-  componentWillMount(){
-  /*
-  *
-  *  This is bad form and the routes need to be moved off to their own config files.
-  *  The question then becomes how do we update loggedIn? Do we need a data store?
-  *
-  */
-    const loggedOut = (
-        <Router>
-          <div className="bodyContainer">
-            <Route exact path="/" render={()=> (
-                <Login callback={this.login}/>
-            )
-            }/>
-          </div>
-        </Router>
-        );
-    const loggedIn = (
-        <Router>
-          <div className="bodyContainer">
-            <Route exact path="/" component={LandingPage}/>
-          </div>
-        </Router>
-        );
-    
-    this.router = this.state.loggedIn? loggedIn : loggedOut;
-  }
-  componentWillUpdate(nextProps, nextState){
-    let self = this;
-    const loggedOut = (
-        <Router>
-          <div className="bodyContainer">
-            <Route exact path="/" render={()=> (
-                <Login callback={(success)=> {
-                  console.log(success); 
-                  self.setState({loggedIn: success})
-                  }
-                }/>
-            )
-            }/>
-          </div>
-        </Router>
-        );
-    const loggedIn = (
-        <Router>
-          <div className="bodyContainer">
-            <Route exact path="/" component={LandingPage}/>
-          </div>
-        </Router>
-        );
-      this.router = nextState.loggedIn? loggedIn : loggedOut;
-  }
-  render() {
-    return (
-      <div className="App">
-        <Header/>
-        {this.router}
-      </div>  
-    );
-  }
+    constructor(){
+        super();
+        this.app = <Simple/>;
+        // if(window.localStorage){
+        //     this.app = <Full/>;
+        // }
+    }
+    render(){
+        let thing = this.app
+        return (thing);
+    }
 }
 
 export default App;
