ba46daa0631a96d1a4f073d5818a3ee543ec23fb
Justin Barclay <justincbarclay@gmail.com>
2017-06-17 10:51:43 -0600

    Allow Users controller to logout user on unauthorized response from server

--- src/views/Users/Users.js
+++ src/views/Users/Users.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Table from '../Table';
 import adminAction from '../../actions/admin';
+import { connect } from 'react-redux';
+import {setAuthState} from '../../actions/auth'; 
 
 class Users extends Component {
   constructor(props) {
@@ -13,28 +15,49 @@ class Users extends Component {
   }
   componentWillMount(){
     let self = this;
-
-    getUsers((data) => {
+    adminAction().index()
+    .then((data) => {
       const users = data.users;
       self.setState({ users: users });
+    })
+    .catch((error) =>{
+      if(error.status === 401){
+        this.props.dispatch(setAuthState(false));
+      }
     });
+    getUsers();
   }
   render() {
     return (
       <div>
+        <SearchBar/>
         <Table header={this.state.headers} data={this.state.users} keys={this.state.keys} title={"Users"} handleClick={this.rowOnClick} baseRoute={"users"}/>
       </div>  
     );
   }
 }
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <div className="animated fadeIn">
+                <div className="row">
+                    <div className="col-sm-11">
+                        <div className="input-group">
+                            <input type="text" className="form-control" placeholder="Search for..." aria-label="Search for..." />
+                            <span className="input-group-btn">
+                                <button className="btn btn-secondary" type="button">Go!</button>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 function getUsers(callback){
-    adminAction()
-    .index()
-    .then(callback)
-    .catch((error) =>{
-      console.log(error);
-    });
+    
 }
 
 //Users = withRouter(Users);
-export default Users;
+export default connect()(Users);
