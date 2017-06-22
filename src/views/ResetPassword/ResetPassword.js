89e96775cb6f213627ae65eff00212ae3b6e8870
Justin Barclay <justincbarclay@gmail.com>
2017-06-21 19:48:13 -0600

    Clean up reset_password view, Rename the button, pull out an error check, change funciton cal

--- src/views/ResetPassword/ResetPassword.js
+++ src/views/ResetPassword/ResetPassword.js
@@ -14,24 +14,27 @@ class ResetPassword extends Component {
     }
     componentWillMount() {
 
-        userActions().get(this.props.match.params.token)
-            .then((data) => {
-                this.setState({ user: data.user });
-            })
-            .catch((error) => {
+        // userActions().get(this.props.match.params.token)
+        //     .then((data) => {
+        //         this.setState({ user: data.user });
+        //     })
+        //     .catch((error) => {
 
-            });
+        //     });
     }
     resetPassword() {
         let password = document.getElementById('password').value;
         let confirmation = document.getElementById('confirmation').value;
-        if (password === confirmation && password.length > 6) {
+        if (password.length < 6){
+            this.setState({messages: ["You password must be 7 characters or longer"], alertClass: "alert-danger"});
+            return;
+        } else if (password === confirmation) {
             let user = {
                 password: password,
                 password_confirmation: confirmation,
                 reset_token: this.props.match.params.token
             }
-            userActions().signup(user)
+            userActions().resetPassword(user)
                 .then((data) => {
                     this.setState({ messages: ["Password was reset"], alertClass: "alert-success" });
                 })
@@ -41,10 +44,6 @@ class ResetPassword extends Component {
         } else {
             this.setState({messages: ["Passwords do not match"], alertClass: "alert-danger"});
         }
-
-    }
-    componentWillUpdate(nextProps, nextState) {
-        document.getElementById('email').innerText += " " + nextState.user.email;
     }
     render() {
         return (
@@ -56,16 +55,13 @@ class ResetPassword extends Component {
                     <Alert alertClass={this.state.alertClass} messages={this.state.messages} />
                     <div className="card-block">
                         <div className="form-group row">
-                            <dd className="col-md-4" id="email">Email: </dd>
-                        </div>
-                        <div className="form-group row">
                             <label className="col-md-1">Password:</label>
                             <input type="password" className="col-lg-5" id="password" placeholder="Password" />
                             <label className="col-md-1">Confirm:</label>
                             <input type="password" className="col-lg-5" id="confirmation" placeholder="Confirm Password" />
                         </div>
                         <div className="btn-group" role="group">
-                            <div onClick={this.signup} className="btn btn-primary btn-md" >Register</div>
+                            <div onClick={this.resetPassword} className="btn btn-primary btn-md" >Reset</div>
                         </div>
                     </div>
                 </div>
