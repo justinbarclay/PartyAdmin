fd000a5bc8e5f71b094064cb72e564e4083f4f47
Justin Barclay <justincbarclay@gmail.com>
2017-07-14 14:24:16 -0600

    Add secondary means to protect routes

---
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const ProtectedRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={props => (
+    localStorage.getItem('jwt') ? (
+      <Component {...props}/>
+    ) : (
+      <Redirect to={{
+        pathname: '/login',
+        state: { from: props.location }
+      }}/>
+    )
+  )}/>
+)
+
+export default ProtectedRoute
